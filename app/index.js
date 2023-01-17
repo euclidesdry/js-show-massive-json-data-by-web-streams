5be313a791b07e5b647466f62d784fea4bdeb6e5
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-17 20:06:54 +0100

    feat: ✨ creating event listeners to start and stop streams

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 const API_URL = 'http://localhost:3000';
+let counter = 0;
 
 async function consumeAPI(signal) {
 	const response = await fetch(API_URL, {
 		signal
 	});
 
-	let counter = 0;
 	const reader = response.body
 		.pipeThrough(new TextDecoderStream())
 		.pipeThrough(parseNDJSON())
@@ -24,13 +24,16 @@ function appendToHTML(element) {
 			const card = `
 			<article>
 				<div class="text">
-					<h3>${title}</h3>
-					<p>${description}</p>
+					<h3>[.${++counter}]${title}</h3>
+					<p>${description.slice(0, 100)}</p>
 					<a href="${url_anime}">Here's Why</a>
 				</div>
 			</article>
 			`;
 			element.innerHTML += card;
+		},
+		abort(reason) {
+			console.log('aborted**', reason)
 		}
 	})
 }
@@ -61,6 +64,16 @@ const [
 	cards
 ] = ['start', 'stop', 'cards'].map(item => document.getElementById(item))
 
-const abortController = new AbortController();
-const readable = await consumeAPI(abortController.signal);
-readable.pipeTo(appendToHTML(cards))
+let abortController = new AbortController();
+
+start.addEventListener('click', async () => {
+	const readable = await consumeAPI(abortController.signal);
+	readable.pipeTo(appendToHTML(cards))
+});
+
+stop.addEventListener('click', async () => {
+	abortController.abort();
+	console.log('aborting...');
+	abortController = new AbortController();
+})
+
--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ createServer(async (request, response) => {
 		// pipeTo is the last step
 		.pipeTo(new WritableStream({
 			async write(chunk) {
-				await setTimeout(1000);
+				// await setTimeout(200);
 				// await setTimeout(5);
 				items++;
 				response.write(chunk);
