3a74f76e5d21c431e80b505f3a4a1b371a5663cf
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-17 18:45:28 +0100

    feat: ✨ mapping only desireable data

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import { createServer } from "node:http";
 import { createReadStream } from "node:fs";
-import { Readable } from "node:stream";
-import { WritableStream } from "node:stream/web";
+import { Readable, Transform } from "node:stream";
+import { WritableStream, TransformStream } from "node:stream/web";
+import { setTimeout } from 'node:timers/promises';
+
+import csvtojson from "csvtojson";
 
 const PORT = 3000;
 
@@ -20,15 +23,34 @@ createServer(async (request, response) => {
 
 	let items = 0;
 	Readable.toWeb(createReadStream('./animeflv.csv')
-	).pipeTo(new WritableStream({
-		write(chunk) {
-			items++;
-			response.write(chunk);
-		},
-		close() {
-			response.end();
-		}
-	}))
+	)
+		// step to step to each individual item witch will travel
+		.pipeThrough(Transform.toWeb(csvtojson()))
+		.pipeThrough(new TransformStream({
+			transform(chunk, controller) {
+				const data = JSON.parse(Buffer.from(chunk));
+				const mappedData = {
+					title: data.title,
+					description: data.description,
+					url_anime: data.url_anime
+				};
+				// line breaker because it's a NDJSON object
+				controller.enqueue(JSON.stringify(mappedData).concat('\n')
+				)
+			}
+		})
+		)
+		// pipeTo is the last step
+		.pipeTo(new WritableStream({
+			async write(chunk) {
+				await setTimeout(1000);
+				items++;
+				response.write(chunk);
+			},
+			close() {
+				response.end();
+			}
+		}))
 
 	response.writeHead(200, headers);
 })
